908e28d5d3aeafe30ef1d325df1077efa423b030
kong <nopaket_kong@hotmail.com>
2018-01-29 15:58:56 +0700

    ok

--- index.php
+++ index.php
@@ -7,7 +7,10 @@
 <body>
    
     <input id="foo" type="text" value="hello">
+	
     <button class="btn" data-clipboard-action="copy" data-clipboard-target="#foo">Copy</button>
+	
+	<button class="btn" data-clipboard-text="hi5">Copy to clipboard</button>
     <script src="clipboard.min.js"></script>
     <script>
     var clipboard = new Clipboard('.btn');
