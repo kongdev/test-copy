90aeb2f3a7dcc946a4c62baaa0a0a8566e78e7f2
kong <nopaket_kong@hotmail.com>
2018-01-29 16:27:50 +0700

    ok

--- index.php
+++ index.php
@@ -1,50 +1,28 @@
+<script src="http://code.jquery.com/jquery-1.12.4.min.js"></script>
 <script>
-window.Clipboard = (function(window, document, navigator) {
-    var textArea,
-        copy;
-
-    function isOS() {
-        return navigator.userAgent.match(/ipad|iphone/i);
-    }
-
-    function createTextArea(text) {
-        textArea = document.createElement("textarea");
-        textArea.value = text;
-        document.body.appendChild(textArea);
-    }
-
-    function selectText() {
-        var range,
-            selection;
-
-        if (isOS()) {
-            range = document.createRange();
-            range.selectNodeContents(textArea);
-            selection = window.getSelection();
-            selection.removeAllRanges();
-            selection.addRange(range);
-            textArea.setSelectionRange(0, 999999);
-        } else {
-            textArea.select();
-        }
-    }
-
-    function copyToClipboard() {        
-        document.execCommand('copy');
-        document.body.removeChild(textArea);
-    }
-
-    copy = function(text) {
-        createTextArea(text);
-        selectText();
-        copyToClipboard();
-    };
-
-    return {
-        copy: copy
-    };
-})(window, document, navigator);
-
-
-Clipboard.copy('text to be copied');
+var clipboard = new Clipboard('.copyUrl');
+               
+                $('.copyUrl').on('click',function() {
+                    var $input = $('#copyIos');
+                    $input.val(share_url);
+                    if (navigator.userAgent.match(/ipad|ipod|iphone/i)) {
+                        clipboard.on('success', function(e) {
+                            e.clearSelection();
+                            $.sDialog({
+                                skin: "red",
+                                content: 'copy success!',
+                                okBtn: false,
+                                cancelBtn: false,
+                                lock: true
+                            });
+                            console.log('copy success!');
+                        });
+                    } else {
+                        $input.select();
+                    }
+                    //document.execCommand('copy');
+                    $input.blur();
+                });
 </script>
+<input id="copyIos" type="hidden" value="">
+<a href="javascript" class="copyUrl">copy</a>
